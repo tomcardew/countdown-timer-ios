64ac34032e7e7d71a3c22df4412d4d7d54f689e0
Andrés Villagómez Ríos <avillagomezr@cinepolis.com>
2021-07-14 16:48:47 -0500

    fixed missing file

---
+++ countdown-timer-ios/ViewModel/MainViewModel.swift
@@ -0,0 +1,96 @@
+//
+//  MainViewModel.swift
+//  countdown-timer-ios
+//
+//  Created by Admin on 14/07/21.
+//
+
+import Foundation
+import CoreData
+
+protocol MainViewModelInput {
+    func viewDidLoad()
+    func addEvent(id: Int, name: String, date: Date)
+    func reorderEvents()
+}
+
+protocol MainViewModelOutput {
+    var title: String { get }
+    var loading: Bool { get }
+    var events: [NSManagedObject] { get }
+    var error: String { get }
+    var eventsDidChange: (() -> Void)? { get set }
+    var loadingDidChange: (() -> Void)? { get set }
+    var errorDidChange: (() -> Void)? { get set }
+}
+
+final class MainViewModel: MainViewModelOutput {
+    
+    let storage = StorageManager.shared
+    
+    var title: String = "My events"
+    var loading: Bool = false {
+        didSet {
+            self.loadingDidChange?()
+        }
+    }
+    var events: [NSManagedObject] = [] {
+        didSet {
+            self.eventsDidChange?()
+        }
+    }
+    var error: String = "" {
+        didSet {
+            self.errorDidChange?()
+        }
+    }
+    
+    // binding handlers
+    var eventsDidChange: (() -> Void)?
+    var loadingDidChange: (() -> Void)?
+    var errorDidChange: (() -> Void)?
+    
+}
+
+extension MainViewModel: MainViewModelInput {
+    
+    func viewDidLoad() {
+        // should fetch all events
+        storage.fetchAll(completion: { result in
+            switch result {
+            case .success(let data):
+                self.events = data
+            case .failure(let error):
+                print(error)
+            }
+        })
+    }
+    
+    func addEvent(id: Int, name: String, date: Date) {
+        self.loading = true
+        storage.save(id: id, name: name, date: date, completion: { result in
+            switch result {
+            case .success(_):
+                self.loading = false
+                break
+            case .failure(let error):
+                self.error = error.localizedDescription
+                self.loading = false
+            }
+        })
+    }
+    
+    func reorderEvents() {
+        var newEvents: [NSManagedObject] = []
+        for event in self.events {
+            if let date = event.value(forKey: "date") as? Date {
+                if date.differenceBetweenInSeconds(from: Date())! >= 0 {
+                    newEvents.append(event)
+                }
+            }
+        }
+        self.events = newEvents
+    }
+    
+    
+}
